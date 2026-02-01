da48cbfcc0a907d38f1fd2df388279b2bb15d3fd
boopot <102613323+boopot@users.noreply.github.com>
2026-02-01 14:25:34 -0500

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dr8eywnho/video/upload/v1769973845/Kabhi_Khushi_Kabhie_Gham_-_Kris_Bowers_Bridgerton_Season_2_Covers_from_the_Netflix_Series_urnbim.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
